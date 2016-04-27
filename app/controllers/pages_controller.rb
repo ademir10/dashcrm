f7485b9779964ed835e2e65f4490499b4d68e210
ademir10 <ddsuportservice@gmail.com>
2016-04-27 19:59:37 -0300

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,86 @@
+class PagesController < ApplicationController
+  #before_action :must_login
+
+  def index
+    @date = DateTime.now.year
+    @categories = Category.order(:position)
+  end
+  
+  #Relatório geral de rendimento dos funcionários separado por perquisa
+  def business_report
+    @datainicial = params[:date1]
+    @datafinal = params[:date2]
+    
+  end
+  
+  #Relatório geral analitico por tipo de venda e data
+  def analitics_report
+    
+    @users = User.where('type_access != ?', 'MASTER').order(:name)
+    if params[:date1].blank?
+      params[:date1] = Date.today
+    end
+    
+    if params[:date2].blank?
+      params[:date2] = Date.today
+    end
+    
+    #se não informar nada, é carregado somentes as vendas do dia
+    if params[:seller].blank? && params[:date1].blank? && params[:date2].blank?
+      @meeting = Meeting.where(status: 'COMPROU').where("updated_at::Date = ?",Date.today).order(:updated_at)
+      @total_meeting = Meeting.where(status: 'COMPROU').where("updated_at::Date ?",Date.today).sum(:cotation_value)
+      
+      @packsearch = Packsearch.where(status: 'COMPROU').where("updated_at::Date ?",Date.today).order(:updated_at)
+      @total_packsearch = Packsearch.where(status: 'COMPROU').where("updated_at::Date ?",Date.today).sum(:cotation_value)
+      
+      @airsearch = Airsearch.where(status: 'COMPROU').where("updated_at::Date ?",Date.today).order(:updated_at)
+      @total_airsearch = Airsearch.where(status: 'COMPROU').where("updated_at::Date ?",Date.today).sum(:cotation_value)
+      
+      @rodosearch = Rodosearch.where(status: 'COMPROU').where("updated_at::Date ?",Date.today).order(:updated_at)
+      @total_rodosearch = Rodosearch.where(status: 'COMPROU').where("updated_at::Date ?",Date.today).sum(:cotation_value)
+
+      
+      @total_geral = @total_meeting.to_f + @total_packsearch.to_f + @total_airsearch.to_f + @total_rodosearch.to_f
+      @total_geral = @total_geral.round(2)
+    end
+    
+    #Se tiver informado somente as datas
+    if params[:seller].blank? && params[:date1].present? && params[:date2].present?
+      @meeting = Meeting.where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).order(:updated_at)
+      @total_meeting = Meeting.where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).sum(:cotation_value)
+      
+      @packsearch = Packsearch.where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).order(:updated_at)
+      @total_packsearch = Packsearch.where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).sum(:cotation_value)
+      
+      @airsearch = Airsearch.where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).order(:updated_at)
+      @total_airsearch = Airsearch.where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).sum(:cotation_value)
+      
+      @rodosearch = Rodosearch.where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).order(:updated_at)
+      @total_rodosearch = Rodosearch.where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).sum(:cotation_value)
+
+      
+      @total_geral = @total_meeting.to_f + @total_packsearch.to_f + @total_airsearch.to_f + @total_rodosearch.to_f 
+      @total_geral = @total_geral.round(2)  
+    end
+    
+    #Se tiver informado o funcionario e as datas
+    if params[:seller].present? && params[:date1].present? && params[:date2].present?
+      @meeting = Meeting.where(clerk_name: params[:seller]).where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).order(:updated_at)
+      @total_meeting = Meeting.where(clerk_name: params[:seller]).where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).sum(:cotation_value)
+      
+      @packsearch = Packsearch.where(user: params[:seller]).where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).order(:updated_at)
+      @total_packsearch = Packsearch.where(user: params[:seller]).where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).sum(:cotation_value)
+      
+      @airsearch = Airsearch.where(user: params[:seller]).where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).order(:updated_at)
+      @total_airsearch = Airsearch.where(user: params[:seller]).where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).sum(:cotation_value)
+      
+      @rodosearch = Rodosearch.where(user: params[:seller]).where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).order(:updated_at)
+      @total_rodosearch = Rodosearch.where(user: params[:seller]).where(status: 'COMPROU').where("updated_at::Date between ? and ?",params[:date1],params[:date2]).sum(:cotation_value)
+
+      
+      @total_geral = @total_meeting.to_f + @total_packsearch.to_f + @total_airsearch.to_f + @total_rodosearch.to_f
+      @total_geral = @total_geral.round(2)
+    end
+  end
+ 
+end
---
+++ config/routes.rb
@@ -0,0 +1,73 @@
+Rails.application.routes.draw do
+  
+  resources :loginfos
+  #pacote de viagens 
+  resources :packsearches do
+    member do
+      get 'manage_pack'
+      post 'update_status_pack'
+    end
+  end
+  #transporte rodoviario
+  resources :rodosearches do
+  member do
+      get 'manage_air'
+      post 'update_status_air'
+     end
+  end  
+  
+  resources :meetings
+  resources :clients
+  resources :documents
+  resources :solutions
+  resources :questions do
+  resources :answers 
+  end
+  
+  #transporte aéreo
+  resources :airsearches do
+    member do
+      get 'manage_air'
+      post 'update_status_air'
+      #get 'end_research'
+    end
+  end
+  resources :categories
+ resources :expire_dates
+ resources :users
+  
+  
+  #para exibir a imagem que foi feito o upload
+  get 'show_file' => 'documents#show_file'
+    
+  root 'pages#index'
+  get 'sessions/new'
+  
+  #rotas para o login
+  get 'expired', to: 'sessions#expired_date'
+  get 'pages/index'
+  get 'login', to: 'sessions#new'
+  delete 'logout', to: 'sessions#destroy'
+  post 'login', to: 'sessions#create'
+  #---------------------------------------------
+  
+  #rotas para contato
+  get 'contact', to: 'messages#new', as: 'contact'
+  post 'contact', to: 'messages#create'
+  #---------------------------------------------
+  #somente para chamar a edição do usuario quando for um membro logado
+  get 'editar_user', to: 'users#chama_edicao'
+  #---------------------------------------------
+  
+  #relatórios
+  #relatorio de analises graficas
+  get 'business_report', to: 'pages#business_report'
+  #relatorio analitico de vendas
+  get 'analitics_report', to: 'pages#analitics_report'
+  
+    
+  #para carregar a view informando que não pode excluir cadastro com relacionamento em outra table
+  get 'message_error_relation_tables', to: 'messages#message_error_relation_tables'
+  end
+
+
